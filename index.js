327640b1c205577eb58a59820c87998aadc10f90
Bushra01-Dhaka <humayraanjum87@gmail.com>
2023-11-06 11:30:20 +0600

    removing .env

--- index.js
+++ index.js
@@ -29,7 +29,9 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+     const jobCollection = client.db('jobHunt').collection('jobs');
 
+     
 
 
 
